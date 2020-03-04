7c6bdc22140c8ac98c2fb586409ca39c0f766629
Rakesh Kumar  Singh <52422856+rakeshkumar1019@users.noreply.github.com>
2020-03-04 12:25:17 +0530

    Create Mirror_Tree.cpp

---
+++ Mirror_Tree.cpp
@@ -0,0 +1,16 @@
+void mirror(Node* node) 
+{
+     // Your Code Here
+     
+     if(node==NULL){
+         return;
+     }
+     mirror(node->left);
+     mirror(node->right);
+     Node* temp;
+     temp=node->left;
+     node->left=node->right;
+     node->right=temp;
+     
+     
+}
